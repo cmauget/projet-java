cb0f321ecde2f1bc2546f33ef8113ee8767da1d3
Clement Mauget <clement.mauget@gmail.com>
2022-05-02 16:18:21 +0200

    creation combi + constructeur

---
+++ Combinaison.java
@@ -0,0 +1,21 @@
+public class Combinaison {
+
+  public enum couleur{rouge, bleu, vert, jaune, orange, blanc, noir, rose};
+  public int taille;
+  public couleur[taille] combi;
+
+  public Combinaison(){
+    this.taille=4;
+    this.combi=couleur[this.taille]
+  }
+
+  public Combinaison(int taille1, couleur[] combi1){
+    this.taille=taille1;
+    this.combi=comni1;
+  }
+
+  public Combinaison setCombi(){
+    
+  }
+
+}
