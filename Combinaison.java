3d89582f680a64cf629bd230d16ca6261588538c
Clement Mauget <clement.mauget@gmail.com>
2022-05-04 09:35:59 +0200

    Ajout Combinaison.java + test ok

--- Combinaison.java
+++ Combinaison.java
@@ -2,20 +2,57 @@ public class Combinaison {
 
   public enum couleur{rouge, bleu, vert, jaune, orange, blanc, noir, rose};
   public int taille;
-  public couleur[taille] combi;
+  public couleur[] combi;
 
   public Combinaison(){
     this.taille=4;
-    this.combi=couleur[this.taille]
+    int i=0;
+    this.combi = new couleur[this.taille];
+    for(i=0;i<taille;i++) {
+      this.combi[i]=couleur.rouge;
+    }
   }
 
   public Combinaison(int taille1, couleur[] combi1){
     this.taille=taille1;
-    this.combi=comni1;
+    int i=0;
+    this.combi = new couleur[this.taille];
+    for(i=0;i<taille;i++) {
+      try{
+          this.combi[i]=combi1[i];
+  			}
+  		catch(ArrayIndexOutOfBoundsException e){
+  			System.out.println("Attention, la taille est trop grande. Prévoir des erreurs");
+      }
+    }
   }
 
-  public Combinaison setCombi(){
-    
+  public void setCombi(couleur[] combi1){
+    int i=0;
+    for(i=0;i<taille;i++) {
+      this.combi[i]=combi1[i];
+    }
+  }
+
+  public String toString(){
+    int i=0;
+    String res="";
+    for(i=0;i<taille;i++) {
+      res=res+this.combi[i]+" ";
+    }
+    return res;
+  }
+
+//test
+  public static void main(String[] args) {
+    Combinaison combi=new Combinaison();
+    couleur[] combitab2=new couleur[]{couleur.bleu, couleur.rouge, couleur.vert};
+    couleur[] combitab3=new couleur[]{couleur.bleu, couleur.rouge, couleur.vert,couleur.rouge};
+    Combinaison combi2=new Combinaison(4, combitab2);
+    System.out.println(combi.toString());
+    combi.setCombi(combitab3);
+    System.out.println(combi.toString());
+    System.out.println(combi2.toString());
   }
 
 }
