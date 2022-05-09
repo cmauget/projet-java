2ebed9fe87f8310f40795e86f01be90195f8fabf
mc <mariecharlotte.gagnard@insa-rouen.fr>
2022-05-09 09:05:48 +0200

    new partie

--- Partie.java
+++ Partie.java
@@ -1,6 +1,55 @@
 public class Partie {
 
-  cacheur : Joueur;
-  chercheur : Joueur;
-  
+  public Joueur cacheur;
+  public Joueur chercheur;
+  public Combinaison combi;
+  public int nbEssais;
+
+  public Partie(){
+    cacheur=joueur();
+    chercheur=joueur();
+    combi=combinaison();
+    nbEssais=0;
+  }
+
+  public Partie(Joueur cach, Joueur cherch, Combinaison comb, int essais){
+    cacheur = cach;
+    chercheur = cherch;
+    combi = comb;
+    nbEssais = essais;
+  }
+
+  public void lancerPartie(){
+
+  }
+
+  public void arreterPartie(){
+
+  }
+
+  public Boolean testCombi(Combinaison test){
+    int i=0;
+    var res = new Boolean(true);
+
+    while ((res)&&(i<combi.taille)){
+      if (test.tab[i] == combi.tab[i]) {
+        res = true;
+        i++;
+      }
+      else {
+        res = false;
+        i++;
+        }
+      }
+
+    return (res);
+  }
+
+  public void setNbEssais(int nbessais){
+    this.nbEssais = nbessais;
+  }
+
+  public int getNbEssais(){
+    return this.nbEssais;
+  }
 }
