a78a1a7f52014d9c0b04de48f4315ad6cc29dbca
Clement Mauget <clement.mauget@gmail.com>
2022-05-11 10:38:29 +0200

    Création HallOfFame, compil ok, à faire : test + ajout score.

--- HallOfFame.java
+++ HallOfFame.java
@@ -1,6 +1,62 @@
 public class HallOfFame {
 
-tabScores = new int[5];
-tabUser = new 
+  public int[] tabScore;
+  public String[] tabNom;
+
+  public HallOfFame(){
+    int i=0;
+    this.tabScore = new int[5];
+    this.tabNom = new String[5];
+    for(i=0;i<5;i++) {
+      this.tabScore[i]=0;
+      this.tabNom[i]="Default";
+    }
+  }
+
+  public HallOfFame(int[] tabScore1, String[] tabNom1){
+    int i=0;
+    for(i=0;i<5;i++) {
+      this.tabScore[i]=tabScore1[i];
+      this.tabNom[i]=tabNom1[i];
+    }
+  }
+
+  public int getScore(int rang){
+    return this.tabScore[rang+1];
+  }
+
+  public String getPseudo(int rang){
+    return this.tabNom[rang+1];
+  }
+
+  public int getScore(String pseudo){ //prend forcèment le meilleur score ?
+    int i=0;                          //renvoie -1 si il n'y est pas
+    int test=0;
+    while (i<5 && test==0) {
+      if (this.tabNom[i]==pseudo){
+        test=1;
+      }
+      i++;
+    }
+    if(test==1){
+      return this.tabScore[i-1];
+    } else {
+      return -1;
+    }
+  }
+
+  public String toString(){
+    String res;
+    int i;
+    res="";
+    for(i=0;i<5;i++) {
+      res=res+" |"+(i+1)+" "+this.tabScore[i]+" "+this.tabNom[i];
+    }
+    return res;
+  }
+
+  //test
+    public static void main(String[] args) {
+    }
 
 }
