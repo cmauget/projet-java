8d9f03b17b69ee7a4f48d87f5f7e5e44db7c9810
mc <mariecharlotte.gagnard@insa-rouen.fr>
2022-05-11 11:49:34 +0200

    Cacheur ok

--- Cacheur.java
+++ Cacheur.java
@@ -1,3 +1,40 @@
-public class Cacheur {
+public class Cacheur extends Joueur{
+
+  public Boolean humain ;
+  public String pseudo;
+  public Combinaison combi;
+
+
+  public Cacheur(){
+    this.humain=false;
+    this.pseudo="NA";
+    this.combi=new Combinaison();
+  }
+
+  public Cacheur(Boolean human,String pseudo){
+    this.humain=human;
+    this.pseudo=pseudo;
+    this.combi=new Combinaison();
+  }
+
+  public String toString(){
+    String res="";
+    String scr = String.valueOf(humain);
+    res = scr;
+    res = res +" " + this.pseudo;
+    res = res + " " + combi.toString();
+    return res;
+  }
+
+  //test
+    public static void main(String[] args) {
+      Cacheur cach1=new Cacheur();
+      Cacheur cach2=new Cacheur(true,"mc");
+      System.out.println(cach1.toString());
+      couleur[] combi2=new couleur[]{couleur.bleu, couleur.rouge, couleur.vert};
+      Combinaison combi1=new Combinaison(3, combi2);
+      cach2.combi=combi1;
+      System.out.println(cach2.toString());
+    }
 
 }
