c5ed67b190d1fe18389493baa1d493159b30ad28
d <diane.sergeant4@gmail.com>
2022-05-11 10:39:57 +0200

    création main pour joueur test créer combi à faire

--- Joueur.java
+++ Joueur.java
@@ -4,32 +4,51 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 
-public class Joueur {
+
+public class Joueur extends Combinaison {
 
   public String pseudo;
   public Combinaison combi;
 
 
-public Joueur(){
-  this.pseudo="";
-}
-
-public Joueur(String pseudo){
-  this.pseudo=pseudo;
-}
-Scanner sc = new Scanner(System.in);
-
-public creerCombi(int combi.taille){
-  int i=0;
-
-  System.out.println("Veuillez entrer votre combinaison svp");
-
-  for(i=0;i<combi.taille;i++)
-  {
-  Scanner sc = new Scanner(System.in);
-  String str = sc.nextLine();
+  public Joueur(){
+    this.pseudo="";
+    this.combi= new Combinaison();
   }
 
-}
+  public Joueur(String pseudo){
+    this.pseudo=pseudo;
+    this.combi= new Combinaison();
+  }
+  Scanner sc = new Scanner(System.in);
 
+  public void creerCombi(int taille){
+    int i=0;
+
+    System.out.println("Veuillez entrer votre combinaison svp");
+
+    for(i=0;i<combi.taille;i++)
+    {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    }
+  }
+
+   public String toString(){
+     int i=0;
+     String res="";
+     res = combi.toString();
+     res=res+" joueur = "+this.pseudo;
+     return res;
+   }
+
+
+  //test
+  public static void main(String[] args){
+    Joueur j1=new Joueur("diane");
+    System.out.println(j1.toString());
+    couleur[] combi2=new couleur[]{couleur.bleu, couleur.rouge, couleur.vert};
+    Combinaison combi=new Combinaison(3, combi2);
+    System.out.println(combi.toString());
+  }
 }
