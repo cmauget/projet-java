0ab286c7b7b98ac239993d794ad2f556089b4072
mc <mariecharlotte.gagnard@insa-rouen.fr>
2022-05-14 15:44:08 +0200

    mastermind niveau ok

--- Mastermind.java
+++ Mastermind.java
@@ -1,6 +1,66 @@
-public class Mastermind{
+import java.util.Scanner;
 
-  static int MAX = 6;
+public class Mastermind {
 
-  
+	public static int taille;
+	public static int maxEssais;
+
+	public Mastermind(){
+		this.taille=0;
+		this.maxEssais=0;
+	}
+
+	public Mastermind(int tail, int mEssais){
+		this.taille=tail;
+		this.maxEssais=mEssais;
+	}
+
+	//saisir niveau du jeu
+	public void saisirNiveau(){
+  	Scanner input=new Scanner(System.in);
+  	char c= '.';
+  	while(!(c == 'n' || c == 'p' || c == 'k'))
+  	{
+   	 System.out.print("Niveau n)ovice p)ro k)iller?");
+	 c=input.next().charAt(0);
+	}
+	  switch (c)
+	    {
+	    case 'n':
+	      this.taille=4;
+	      this.maxEssais=10;
+	      //System.out.println(this.maxEssais + " " + this.taille);
+	      break;
+	    case 'p':
+	      this.taille=5;
+	      this.maxEssais=12;
+	      //System.out.println(this.maxEssais + " " + this.taille);
+	      break;
+	    case 'k':
+	      this.taille=6;
+	      this.maxEssais=15;
+	      //System.out.println(this.maxEssais + " " + this.taille);
+	      break;
+	    }
+	}
+
+
+//	texte daccueil du jeu
+//	public void bienvenu(int combi.taille, int maxCoups){
+//	System.out.println("Pouvez-vous trouver ma combinaison de " + combi.taille +" symboles");
+//	System.out.println("[chiffres entre 1 et 7 avec repetitions possibles]");
+//	System.out.println("en moins de "+ maxEssais +" coups? Entrez les symboles des ");
+//	System.out.println("propositions terminees par [Entree].");
+//	System.out.println("(# un bien place, o un mal place)" + "\n");
+//	}
+
+
+	//Jeu du mastermind
+
+
+  	public static void main(String[] args){
+  	  Mastermind mast = new Mastermind();
+    	int nbEssais=0;
+    	mast.saisirNiveau();
+    }
 }
--- Partie.java
+++ Partie.java
@@ -14,7 +14,7 @@ public class Partie {
     nbEssais=0;
   }
 
-  public Partie(Joueur cach, Joueur cherch, Combinaison comb, int essais){
+  public Partie(Cacheur cach, Chercheur cherch, Combinaison comb, int essais){
     cacheur = cach;
     chercheur = cherch;
     combi = comb;
@@ -36,9 +36,6 @@ public class Partie {
 
       int nbp = 0;
       int nmp = 0;
-
-      etudierCombi();
-      trouve = (nbp[0] == combi.taille)
 
       //resultat du code
   		for (int j=1; j <= nbp; j++)
@@ -65,47 +62,13 @@ public class Partie {
       }
     }
 
-    //saisir niveau du jeu
-	public static void saisirNiveau(){
-  	Scanner input=new Scanner(System.in);
-  	char c= '.';
-  	while(!(c == 'n' || c == 'p' || c == 'k'))
-  		{
-  		System.out.print("Niveau n)ovice p)ro k)iller?");
-  		c=input.next().charAt(0);
-  		}
-  	switch (c)
-  		{
-  		case 'n':
-  			combi.taille=4;
-  			maxEssais=10;
-  			break;
-  		case 'p':
-  			combi.taille=5;
-  			maxEssais=12;
-  			break;
-  		case 'k':
-  			combi.taille=6;
-  			maxEssais=15;
-  			break;
-  		}
-	}
-
-  //texte daccueil du jeu
-	public static void bienvenu(int combi.taille, int maxCoups){
-	System.out.println("Pouvez-vous trouver ma combinaison de " + combi.taille +" symboles");
-	System.out.println("[chiffres entre 1 et 7 avec repetitions possibles]");
-	System.out.println("en moins de "+ maxEssais +" coups? Entrez les symboles des ");
-	System.out.println("propositions terminees par [Entree].");
-	System.out.println("(# un bien place, o un mal place)" + "\n");
-	}
-
   public void arreterPartie(){
-
+    //trouver bonne combi
+    //trop d'essais
   }
 
-  public int BIEN_PLACE = -1;
-  public int MAL_PLACE=0;
+//  public int BIEN_PLACE = -1;
+  //public int MAL_PLACE=0;
 
   public int trouverBienPlaces(Combinaison test){
     int nbp=0;
@@ -167,12 +130,5 @@ public class Partie {
     return this.nbEssais;
   }
 
-  //Jeu du mastermind
-	public static void mastermind2()
-	{
-	int[] n = new int[1], m = new int[1], maxcoups = new int[1];
-	saisirNiveau();
-	bienvenu();
-	mastermind(n[0],m[0],maxcoups[0]);
-	}
+
 }
